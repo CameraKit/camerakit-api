0ca428245416366baefd510958211974163cfb90
Kyle <Timone@users.noreply.github.com>
2018-08-15 16:15:07 -0400

    Add basic splash screen

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, FastifyAdapter } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  app.useStaticAssets({
+    prefix: '/',
+    root: path.join(__dirname + "/../dist/public")
+  });
   await app.listen(3000);
 }
 bootstrap();
