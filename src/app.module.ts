5a5653c8b1a500f42b01a003c470ce730ac41aa3
Kyle Trusler <kyletrusler@gmail.com>
2018-08-16 14:19:53 -0400

    Add apollo server connection
    add graphql and update current packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { Connection } from 'typeorm';
 import { UserModule } from './user/user.module';
+import { ApolloServer } from 'apollo-server-express';
+import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule], // get config from ormconfig.json,
+  imports: [TypeOrmModule.forRoot(), UserModule, GraphQLModule], // get config from ormconfig.json,
 })
 
 export class AppModule {
-  constructor(private readonly connection: Connection) {}
+  constructor(private readonly connection: Connection, private readonly graphQLFactory: GraphQLFactory) { }
+
+  configureGraphQl(app: any, path: string) {
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');
+    const schema = this.graphQLFactory.createSchema({ typeDefs });
+
+    const server = new ApolloServer({ schema });
+    server.applyMiddleware({ app, path });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as path from 'path';
 
@@ -7,8 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.useStaticAssets({
     prefix: '/',
-    root: path.join(__dirname + "/../dist/public")
+    root: path.join(__dirname + '/../dist/public'),
   });
+
+  const appModule = app.get(AppModule);
+  const apiPath = '/api/graphql';
+  appModule.configureGraphQl(app, apiPath);
+
   await app.listen(3000);
 }
 bootstrap();
