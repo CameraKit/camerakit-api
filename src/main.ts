551f15a3a02ce087097bcbd6ce768b4f4d4e08f8
Kyle <Timone@users.noreply.github.com>
2018-08-17 18:51:32 -0400

    Add CORS
    change API port to 3001 since website is on 3000

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as path from 'path';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
@@ -8,11 +9,12 @@ async function bootstrap() {
     prefix: '/',
     root: path.join(__dirname + '/../dist/public'),
   });
+  app.use(cors());
 
   const appModule = app.get(AppModule);
   const apiPath = '/api/graphql';
   appModule.configureGraphQl(app, apiPath);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
