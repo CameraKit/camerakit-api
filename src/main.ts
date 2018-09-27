6745f4c0b51c5ce0caf556a1b1ef1cd67dcd8796
emersoncloud <emerson.cloud@gmail.com>
2018-09-27 14:35:23 -0400

    Enable CORS to only allow specified origins

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,23 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.configure && dotenv.configure();
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
   app.useStaticAssets({
     prefix: '/',
     root: path.join(__dirname + '/../dist/public'),
   });
-  app.use(cors());
+  
+  app.use(cors({
+    'origin': process.env.ALLOWED_ORIGINS,
+    'methods': process.env.ALLOWED_METHODS,
+    'preflightContinue': false,
+    'optionsSuccessStatus': 204,
+  }));
 
   const appModule = app.get(AppModule);
   const apiPath = '/api/graphql';
-  appModule.configureGraphQl(app, apiPath);
-
+  appModule.configureGraphQl(app, apiPath);      
+ 
   await app.listen(3001);
 }
 bootstrap();
