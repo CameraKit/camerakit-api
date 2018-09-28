616e962c52d4555f4becfb6dead0061573be843a
emersoncloud <emerson.cloud@gmail.com>
2018-09-28 00:25:59 -0400

    Use AWS to send emails

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -3,17 +3,17 @@ import { Contact } from './contact.entity';
 
 import * as nodemailer from 'nodemailer';
 import * as Email from 'email-templates';
+import * as aws from 'aws-sdk';
+import * as path from 'path';
+
+aws.config.loadFromPath(path.join(__dirname, '../../config.json'));
 
 @Injectable()
 export class ContactService {
   private transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD
-    }
+    SES: new aws.SES({
+      apiVersion: '2010-12-01'
+    })
   });
 
   private email = new Email({
