8f21d26dabc1f65f169db74385a49181ebdc3b14
Kyle <Timone@users.noreply.github.com>
2018-10-17 00:12:28 -0400

    Fix incorrect path

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { Payment } from './payment.entity';
-import { SubscriptionModule } from 'subscription/subscription.module';
+import { SubscriptionModule } from '../subscription/subscription.module';
 import { ProductModule } from 'product/product.module';
 
 @Module({
